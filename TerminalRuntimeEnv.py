b34b846d6952f92d2b3cff2028f7efdca0c53525
WooHooASDF <daniel.leviss@outlook.com>
2021-01-09 17:26:14 +0000

    Added new command to Commands.py, mkdir. changed command checking method in TerminalRuntimeEnv.py to be easier to add to

--- TerminalRuntimeEnv.py
+++ TerminalRuntimeEnv.py
@@ -2,16 +2,6 @@ import Commands
 
 """V1"""
 
-def checkForCommand(entry):
-    if entry == ("l", "s"):
-        Commands.ls()
-    elif entry[:2] == ("c", "d"):
-        path = transformToString(entry[3:])
-        Commands.cd(path)
-    elif entry == ("w", "h", "o", "a", "m", "i"):
-        print(Commands.whoami())
-    elif entry[:3] == ("p", "w", "d"):
-        print(Commands.pwd())
 
 def transformToString(tuple):
     output = ""
@@ -19,16 +9,38 @@ def transformToString(tuple):
         output += x
     return output
 
-def transfromToTuple(str):
+def transformToTuple(str):
     output = []
     for x in str:
         output.append(x)
     return tuple(output)
 
 
+
+def checkForCommand(entry):
+    #if entry == ("l", "s"):
+    if entry == transformToTuple("ls"):
+        Commands.ls()
+    #elif entry[:2] == ("c", "d"):
+    elif entry[:2] == transformToTuple("cd"):
+        path = transformToString(entry[3:])
+        Commands.cd(path)
+    #elif entry == ("w", "h", "o", "a", "m", "i"):
+    elif entry == transformToTuple("whoami"):
+        print(Commands.whoami())
+    #elif entry == ("p", "w", "d"):
+    elif entry == transformToTuple("pwd"):
+        print(Commands.pwd())
+    #elif entry[:3] == ("m", "k", "d", "i", "r"):
+    elif entry[:5] == transformToTuple("mkdir"):
+        path = transformToString(entry[6:])
+        Commands.mkdir(path)
+
+
+
 #Main Loop NOT TO RUN ON IMPORT
 if __name__ == "__main__":
     while True:
         entry = input(Commands.pwd() + " >>>")
-        checkForCommand(transfromToTuple(entry))
+        checkForCommand(transformToTuple(entry))
 
